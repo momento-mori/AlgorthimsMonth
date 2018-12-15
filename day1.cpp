5623e4c0772d3e9e09a1311fb7b074781804d79d
momento-mori <filbelay.fb@gmail.com>
2018-12-15 16:46:25 -0600

    Merge LinkedList: Attempt1

--- day1.cpp
+++ day1.cpp
@@ -1,3 +1,13 @@
+/*Given two singly linked lists. 
+  Return sorted list of both lists.
+*/
+
+/*
+  Approach: 
+  Questions: Will numbers be sorted within provided lists.
+  	     
+*/
+
 #include <iostream>
 #include <cstidlib>
 
@@ -11,15 +21,24 @@ ListNode *mergeLists(ListNode *l1, ListNode *l2)
 {
   ListNode *result;
   while(l1 && l2){
-
+    if(l1-val <= l2->val){
+      result->next = l1;
+      l1 = l1->next;
+    }
+    else{
+      result->next = l2;
+      l2 = l2->next;
+    }
   }
 
   while(l1){
-
+    result->next = l1;
+    l1 = l1->next;
   }
 
   while(l2){
-
+    result->next = l2; 
+    l2 = l2->next
   }
   return result;
 }
@@ -30,3 +49,7 @@ int main()
   ListNode *l2;
 
 }
+
+/*-----------------------------------------------------------------------------
+
+-----------------------------------------------------------------------------*/
