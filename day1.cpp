59048cdb821d73b35565a11ab88ca0ce37ba4562
momento-mori <filbelay.fb@gmail.com>
2018-12-15 16:37:34 -0600

    Day 1. Shit github

---
+++ day1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstidlib>
+
+struct ListNode{
+  int val;
+  ListNode *next;
+  ListNode(int v) : val(v), next(NULL) { }
+};
+
+ListNode *mergeLists(ListNode *l1, ListNode *l2)
+{
+  ListNode *result;
+  while(l1 && l2){
+
+  }
+
+  while(l1){
+
+  }
+
+  while(l2){
+
+  }
+  return result;
+}
+
+int main()
+{
+  ListNode *l1;
+  ListNode *l2;
+
+}
