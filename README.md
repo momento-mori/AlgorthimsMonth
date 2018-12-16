b31f13efdcdf2ea38b5c5223094a12c6eb567ad6
momento-mori <filbelay.fb@gmail.com>
2018-12-15 18:52:51 -0600

    Day1 complete.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # AlgorthimsMonth
 Data Structure and Algorithim Every Day
+
+1. Merge two sorted singly linkedlists.
--- day1.cpp
+++ day1.cpp
@@ -1,7 +1,14 @@
-/*Given two singly linked lists. 
-  Return sorted list of both lists.
-*/
+/*Given sorted two singly linked lists. 
+  Return sorted list of both lists merged.
 
+	Input
+    [ 1 2 4]  [ 1 3 4]
+	Ouput
+    [1 1 2 3 4 4]
+  
+  * Attempt 1 Output:
+      [1 4]
+------------------------------------------------------------------------------*/
 /*
   Approach: 
   Questions: Will numbers be sorted within provided lists.
@@ -20,6 +27,9 @@ struct ListNode{
 ListNode *mergeLists(ListNode *l1, ListNode *l2)
 {
   ListNode *result;
+  ListNode sentinel(0)
+  result = &sentinel;
+
   while(l1 && l2){
     if(l1-val <= l2->val){
       result->next = l1;
@@ -31,16 +41,8 @@ ListNode *mergeLists(ListNode *l1, ListNode *l2)
     }
   }
 
-  while(l1){
-    result->next = l1;
-    l1 = l1->next;
-  }
-
-  while(l2){
-    result->next = l2; 
-    l2 = l2->next
-  }
-  return result;
+  result->next = l1 ? l1 : l2;
+  return sentinel.next;
 }
 
 int main()
@@ -51,5 +53,14 @@ int main()
 }
 
 /*-----------------------------------------------------------------------------
+  Reflections:
 
+  Orginal approach was correct. Didn't grasp concept of needing a sentinle head 
+  node in the list we will merge into. Needed cause if you just have pointer and  you try to access it through *next you lose nodes you connected in the last 
+  iteration.
+
+  The ternary operation was exactly right. 
+
+  Only gotch is you have to return the resulitng merged node wihtout the 
+  sentinle.
 -----------------------------------------------------------------------------*/
