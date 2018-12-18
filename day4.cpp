2d9b16127db52fa7d9c0bc21fff4c16b6f3a54f1
momento-mori <filbelay.fb@gmail.com>
2018-12-18 10:50:11 -0600

    day 4. dp

---
+++ day4.cpp
@@ -0,0 +1,19 @@
+/*
+   Mininmum Swaps to Make Sequences Increasing
+
+   Given 2 integer sequences that are guarenteed to be made into strictly
+   increasing sequences. Whats the minumum number of swaps between indexes
+   sequences needed to make both strictly increasing.
+*/
+
+#include <iostream>
+
+int minSwap(std::vector<int> &A, std::vector<int> &B)
+{
+
+}
+
+int main()
+{
+
+}
