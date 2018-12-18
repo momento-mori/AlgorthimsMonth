f9106a512448cfef390441faca401c9e82b97f71
momento-mori <filbelay.fb@gmail.com>
2018-12-18 13:41:31 -0600

    worked on it w esam. Analysis began.

--- day4.cpp
+++ day4.cpp
@@ -4,9 +4,12 @@
    Given 2 integer sequences that are guarenteed to be made into strictly
    increasing sequences. Whats the minumum number of swaps between indexes
    sequences needed to make both strictly increasing.
+
+   Note that the swaps must be between A[i] and B[i]
 */
 
 #include <iostream>
+#include <vector>
 
 int minSwap(std::vector<int> &A, std::vector<int> &B)
 {
@@ -17,3 +20,15 @@ int main()
 {
 
 }
+/*------------------------------------------------------------------------------
+   Analysis.
+ 
+  * Since we're asked for the minimum number of swaps we need to maintain 
+    how many time we needed to make a swap to make the list strinctly 
+    increasing and how many swaps we DIDNT make. The minimum of these two
+    values will tell us the answer.
+
+  * Clever think to do here is use vectors for swap and no_swap that way we 
+    can pass std::min(swap.back(), no_swap.back()) To use an idomatic way of 
+    finding mininum of two lists.
+------------------------------------------------------------------------------*/
