aa2500d23a1c53fe5eae0985d47343262da5ea76
momento-mori <filbelay.fb@gmail.com>
2018-12-20 15:30:04 -0600

    day6 problem statement.

---
+++ day6.cpp
@@ -0,0 +1,22 @@
+/*
+   All Paths From Source to Target
+    Given source is vertex 0 and target is vertex N-1.
+
+   Input: [[1,2[, [3], [3], []]
+   Output: [[0,1,3], [0,2,3]]
+   Explanation: The graph looks like this:
+   
+   0-->1
+   |   |
+   v   v
+   2-->3
+
+   There are two paths: 0->1->3 and 0->2->3
+
+   Given graph is directed acyclic graph of N nodes.
+    What algorthims work best on directed graphs?
+
+    Acyclic?
+
+    
+*/
