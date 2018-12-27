2208d6d60d8a429b3a89d75eaa0bfc99a89a46aa
momento-mori <filbelay.fb@gmail.com>
2018-12-27 01:12:20 -0600

    day9 started. needs day10 as ramp up.

---
+++ day9.cpp
@@ -0,0 +1,24 @@
+/*
+      646. Maximum Length of Pair Chain
+  Given n pairs of numbers. In every pair, the first # is always smaller
+  than the second number.
+
+   Input:
+  [[1 2] [2 3] [3 4]]
+   Output:
+  2
+
+  ** Your brain should of mapped this to longest common subsequence **
+
+*/
+
+#include <iostream>
+#include <vector>
+
+int findLongestChain(std::vector<std::vector<int>> &pairs)
+{
+  int result; 
+}
+int main()
+{
+}
