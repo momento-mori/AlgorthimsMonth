e22bfe4e996533b1ebe17f106c9723b51cf53b39
momento-mori <filbelay.fb@gmail.com>
2018-12-30 03:31:33 -0600

    day14 two sum 4.

---
+++ day14.cpp
@@ -0,0 +1,34 @@
+/*  12.29.2018
+
+    653. Two Sum 4 - Input is BST
+  Given a Binary Search Tree and a target number, return true if there exist
+  two elements in the BST such that their sum is equal to the given target.
+  
+    Approach:
+  Insert each value in tree into hash map thats passed to helper function.
+    
+
+*/
+
+#include <iostream>
+#include <map>
+struct TreeNode{
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreenNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+
+
+bool findTarget(TreeNode *root, int k)
+{
+}
+
+int main()
+{
+}
+
+/*
+
+*/
